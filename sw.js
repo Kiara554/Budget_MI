227ae73a72f6323f2ae95bc9933825609e3efe31
Kiara554 <kiara.faisy@gmail.com>
2026-05-02 01:12:55 +0200

    ajouter un onglet pour le cash

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mi-depenses-v22';
+const CACHE = 'mi-depenses-v23';
 const CORE = ['./','./index.html','./manifest.json','./icon.svg','./favicon.svg'];
 
 self.addEventListener('install', e => {
